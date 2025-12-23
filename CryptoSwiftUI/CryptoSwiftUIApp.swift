881cb24262c96e6132d6aeb9c5663cdd51c85850
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2025-12-23 16:56:43 -0500

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Antony Huaman Alikhan on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
